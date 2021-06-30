96a64cadecec96b18f9503bf5776b4fc69d25739
Rohan Sachdeva <rohan.sachdeva1990@gmail.com>
2021-06-30 23:24:52 +0200

    First Program

---
+++ test-project/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace test_project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Rohan To C# World!");
+
+            string firstName, lastName;
+            Console.Write("Enter your first name: ");
+            firstName = Console.ReadLine();
+
+            Console.Write("Enter your last name: ");
+            lastName = Console.ReadLine();
+
+            Console.Write("Your full name is " + firstName + " " + lastName);
+        }
+    }
+}
